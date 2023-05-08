d3f8b2f74645c7146d1e43ece45b8cfd8ef81acc
DESKTOP-BBDMNJN\Dima <ryabchikk@rambler.ru>
2023-05-09 00:40:06 +0300

    изменение метода Add в DevicesController

--- HomeApi/HomeApi/Controllers/DevicesController.cs
+++ HomeApi/HomeApi/Controllers/DevicesController.cs
@@ -39,6 +39,13 @@ namespace HomeApi.Controllers
         [Route("Add")]
         public IActionResult Add([FromBody] AddDeviceRequest request)// Атрибут, указывающий, откуда брать значение объекта + // Объект запроса
         {
+            if (request.CurrentVolts < 120)
+            {
+                // Добавляем для клиента информативную ошибку
+                ModelState.AddModelError("currentVolts", "Устройства с напряжением меньше 120 вольт не поддерживаются!");
+                return BadRequest(ModelState);
+            }
+
             return StatusCode(200, $"Устройство {request.Name} добавлено!");
         }
     }
